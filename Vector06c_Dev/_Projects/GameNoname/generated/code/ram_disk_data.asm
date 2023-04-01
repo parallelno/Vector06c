34f2978ab95abb98180592502ae543b113add3aa
parallelno <mailforfriendparallelno@gmail.com>
2023-03-31 22:23:02 -0700

    added an option to export sprites with no preshifting

--- Vector06c_Dev/_Projects/GameNoname/generated/code/ram_disk_data.asm
+++ Vector06c_Dev/_Projects/GameNoname/generated/code/ram_disk_data.asm
@@ -47,8 +47,8 @@ chunk_bank3_addr8000_0:
 ;                             hero_r_sprites [14466], skeleton_sprites [10140], scythe_sprites [1626], hero_sword_sprites [5226], bomb_sprites [960], 
 ; bank0 addr8000 [27837 free] description: 
 ;                             level01_data [795], backs_sprites [496], decals_sprites [3640], 
-; bank1 addr0    [ 2854 free] description: 
-;                             knight_sprites [16398], burner_sprites [9600], fx_sprites [3660], 
+; bank1 addr0    [ 5818 free] description: 
+;                             knight_sprites [16398], burner_sprites [9600], fx_sprites [696],
 ; bank1 addr8000 [    0 free] description: $8000-$9FFF tiledata buffer (collision, copyToScr, etc), $A000-$FFFF back buffer2 (to restore a background in the back buffer)
 ; bank2 addr0    [10162 free] description: 
 ;                             hero_l_sprites [14466], vampire_sprites [7884], 
@@ -56,9 +56,9 @@ chunk_bank3_addr8000_0:
 ;                             song01 [12542], sound_rd [705], 
 ; bank3 addr0    [25880 free] description: 
 ;                             level01_gfx [6632], 
-; bank3 addr8000 [ 5996 free] description: $8000-$9FFF code library. $A000-$FFFF back buffer
-;                             sprite_rd [173], draw_sprite_rd [340], draw_sprite_hit_rd [318], draw_sprite_invis_rd [26], utils_rd [87], sprite_preshift_rd [1538], 
-; [92344 total free]
+; bank3 addr8000 [ 5993 free] description: $8000-$9FFF code library. $A000-$FFFF back buffer
+;                             sprite_rd [173], draw_sprite_rd [340], draw_sprite_hit_rd [318], draw_sprite_invis_rd [26], utils_rd [87], sprite_preshift_rd [1541], 
+; [95305 total free]
 
 .if BUFFERS_START_ADDR < ram_disk_data
 			.error "the programm is too big. It overlaps with tables at the end of RAM"
