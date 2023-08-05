be0a7808d96d594b5e7b5a140dbaac5739fb9097
parallelno <mailforfriendparallelno@gmail.com>
2023-08-05 16:10:11 -0700

    fixed: tileset path to work on Mac. Storytelling is in WIP

--- Vector06c_Dev/_Projects/GameNoname/source/levels/level00.tsx
+++ Vector06c_Dev/_Projects/GameNoname/source/levels/level00.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.0" name="village00" tilewidth="16" tileheight="16" tilecount="512" columns="16">
- <image source="art\\level00.png" width="256" height="512"/>
+ <image source="art/level00.png" width="256" height="512"/>
 </tileset>
--- Vector06c_Dev/_Projects/GameNoname/source/levels/level01.tsx
+++ Vector06c_Dev/_Projects/GameNoname/source/levels/level01.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.0" name="dungeon01" tilewidth="16" tileheight="16" tilecount="512" columns="16">
- <image source="art\\level01.png" width="256" height="512"/>
+ <image source="art/level01.png" width="256" height="512"/>
 </tileset>
