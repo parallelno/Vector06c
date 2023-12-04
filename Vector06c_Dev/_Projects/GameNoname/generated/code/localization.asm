735d2ea08f263f34464993d2f91c6fd696986ab0
parallelno <mailforfriendparallelno@gmail.com>
2023-12-04 09:46:39 -0800

    delete temp files

--- Vector06c_Dev/_Projects/GameNoname/generated/code/localization.asm
+++
@@ -1,1 +0,0 @@
-LOCALIZATION = 1
--- Vector06c_Dev/_Projects/GameNoname/generated/code/segment_bank1_addr8000.asm
+++
@@ -1,9 +0,0 @@
-.org $8000
-.include "asm\\globals\\global_consts.asm"
-RAM_DISK_S = RAM_DISK_S1
-RAM_DISK_M = RAM_DISK_M1
-
-__vampire_sprites_rd_data_start:
-.include "generated\\sprites\\vampire_sprites.asm"
-.align 2
-__vampire_sprites_rd_data_end:
